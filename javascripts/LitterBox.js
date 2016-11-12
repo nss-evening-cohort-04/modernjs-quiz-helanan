e630acc66f1c281052da3fbaa8c71b7bf14461a9
Helana Nosrat <helanan@gmail.com>
2016-11-12 11:54:49 -0600

    finished functionality and styling, quiz completed

--- javascripts/LitterBox.js
+++ javascripts/LitterBox.js
@@ -1,154 +1,152 @@
-"use strict";
+// "use strict";
 
-var CatBotFight = (function (LitterBox){
+// let selectedCatbot = null;
+// let selectedWeapon = null
 
-let selectedCatbot = null;
-let selectedWeapon = null
+// let litterBox = function() {
+// 	this.player1 = null;
+// 	this.player2 = null;
+// 	this.catbotAttackText = []; //battletext
+// 	this.player1StartHealth = 0;
+// 	this.player2StartHealth = 0;
+// };
 
-let litterBox = function() {
-	this.player1 = null;
-	this.player2 = null;
-	this.catbotAttackText = []; //battletext
-	this.player1StartHealth = 0;
-	this.player2StartHealth = 0;
-};
+// $(document).ready(function() {
 
-$(document).ready(function() {
+// 	const $player1Name = $('#robot1Name');
+// 	const $player2Name = $('#robot2Name');
+// 	const $catFightText = $('#catFightText');//combat
 
-	const $player1Name = $('#robot1Name');
-	const $player2Name = $('#robot2Name');
-	const $catFightText = $('#catFightText');//combat
-
-	//attack
-	function attack() {
-		litterBox.Player1.catbotType.totalDamage = litterBox.Player1.catbotType.baseDamage + litterBox.Player1.weaponType.damage + Math.floor(Math.random() * 500) + 25;
-		litterBox.Player2.catbotType.totalDamage = litterBox.Player2.catbotType.baseDamage + litterBox.Player2.weaponType.damage + Math.floor(Math.random() * 500) + 25;
+// 	//attack
+// 	function attack() {
+// 		litterBox.Player1.catbotType.totalDamage = litterBox.Player1.catbotType.baseDamage + litterBox.Player1.weaponType.damage + Math.floor(Math.random() * 500) + 25;
+// 		litterBox.Player2.catbotType.totalDamage = litterBox.Player2.catbotType.baseDamage + litterBox.Player2.weaponType.damage + Math.floor(Math.random() * 500) + 25;
 		
-		litterBox.Player1.catbotType.life -= litterBox.Player2.catbotType.totalDamage;
-  		litterBox.Player2.catbotType.life -= litterBox.Player1.catbotType.totalDamage;
+// 		litterBox.Player1.catbotType.life -= litterBox.Player2.catbotType.totalDamage;
+//   		litterBox.Player2.catbotType.life -= litterBox.Player1.catbotType.totalDamage;
 		
-		$playerText.append(`<div>${litterBox.Player1.playerName} hits for ${litterBox.Player1.catbotType.totalDamage} damage.</div>`);
-  		$playerText.append(`<div>${litterBox.Player2.playerName} hits for ${litterBox.Player2.catbotType.totalDamage} damage.</div>`);
+// 		$playerText.append(`<div>${litterBox.Player1.playerName} hits for ${litterBox.Player1.catbotType.totalDamage} damage.</div>`);
+//   		$playerText.append(`<div>${litterBox.Player2.playerName} hits for ${litterBox.Player2.catbotType.totalDamage} damage.</div>`);
 
-  		$('#player1Life').html(`${litterBox.Player1.catbotType.life}`);
-  		$('#player2Life').html(`${litterBox.Player2.catbotType.life}`);
+//   		$('#player1Life').html(`${litterBox.Player1.catbotType.life}`);
+//   		$('#player2Life').html(`${litterBox.Player2.catbotType.life}`);
 	
-  		winner(litterBox);
-	}
+//   		winner(litterBox);
+// 	}
 
-function winner(litterBox){
-	console.log("litterBox", litterBox);
-	if(litterBox.Player1.catbotType.life < 1){
-		$catFightText.append(`<div><h1>${litterBox.Player1.playerName}'s  ${litterBox.Player2.catbotType.type} ${litterBox.Player2.catbotType.name} defeats ${litterBox.Player1.playerName}'s ${litterBox.Player1.catbotType.type} ${litterBox.Player1.catbotType.name} with a ${litterBox.Player2.weaponType.weapon}!</h1></div>`);
-	}
-	else if(LitterBox.Player2.catbotType.life < 1){
-		$catFightText.append(`<div><h1>${litterBox.Player1.playerName}'s ${litterBox.Player1.catbotType.type} ${litterBox.Player1.catbotType.name} defeats ${litterBox.Player2.playerName}'s ${litterBox.Player2.catbotType.type} ${litterBox.Player2.catbotType.name} with a ${litterBox.Player1.weaponType.weapon}!</h1></div>`);
-	}
-}
+// function winner(litterBox){
+// 	console.log("litterBox", litterBox);
+// 	if(litterBox.Player1.catbotType.life < 1){
+// 		$catFightText.append(`<div><h1>${litterBox.Player1.playerName}'s  ${litterBox.Player2.catbotType.type} ${litterBox.Player2.catbotType.name} defeats ${litterBox.Player1.playerName}'s ${litterBox.Player1.catbotType.type} ${litterBox.Player1.catbotType.name} with a ${litterBox.Player2.weaponType.weapon}!</h1></div>`);
+// 	}
+// 	else if(LitterBox.Player2.catbotType.life < 1){
+// 		$catFightText.append(`<div><h1>${litterBox.Player1.playerName}'s ${litterBox.Player1.catbotType.type} ${litterBox.Player1.catbotType.name} defeats ${litterBox.Player2.playerName}'s ${litterBox.Player2.catbotType.type} ${litterBox.Player2.catbotType.name} with a ${litterBox.Player1.weaponType.weapon}!</h1></div>`);
+// 	}
+// }
 
-setInterval (() => {
-	$('#catFightText').children().first().fadeOut(1000).remove();
-}, 3000);
+// setInterval (() => {
+// 	$('#catFightText').children().first().fadeOut(1000).remove();
+// }, 3000);
 
-//call attack
-$(".attackButton").click(function(e) {
-	attack();
-});
+// //call attack
+// $(".attackButton").click(function(e) {
+// 	attack();
+// });
 
-function setupLitterBox() {
-	$catFightText.text("");
-$player1Name.text(litterBox.Player1.playerName);
-$player2Name.text(litterBox.Player2.playerName);
-$('#robot1Type').html(`${litterBox.Player1.catboxType.name}`);
-$('#robot2Type').html(`${litterBox.Player2.catboxType.name}`);
-$('#player1Life').html(`${litterBox.Player1.catboxType.life}`);
-$('#player2Life').html(`${litterBox.Player2.catboxType.life}`);
-}
+// function setupLitterBox() {
+// 	$catFightText.text("");
+// $player1Name.text(litterBox.Player1.playerName);
+// $player2Name.text(litterBox.Player2.playerName);
+// $('#robot1Type').html(`${litterBox.Player1.catboxType.name}`);
+// $('#robot2Type').html(`${litterBox.Player2.catboxType.name}`);
+// $('#player1Life').html(`${litterBox.Player1.catboxType.life}`);
+// $('#player2Life').html(`${litterBox.Player2.catboxType.life}`);
+// }
 
 
-$(document).on("click", ".robotList1", function(e) {
-	let selectedCatbot = $(this).text();
-	let player1Name = $("#player1Name").val();
+// $(document).on("click", ".robotList1", function(e) {
+// 	let selectedCatbot = $(this).text();
+// 	let player1Name = $("#player1Name").val();
 
-	var selectedcatbot = new CatBotFight.litterBox[selectedCatbot]();
+// 	var selectedcatbot = new CatBotFight.litterBox[selectedCatbot]();
 
-	CatBotFight.Player1 = new CatBotFight.Player(player1Name,selectedcatbot);
-	$('#weaponList1').html(`${litterBox.Player1.catbotType.name} Selected
-    		<div class="dropdown">
-  				<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" 	
-  					data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-    						Choose a Weapon
-    				<span class="caret"></span>
-  				</button>
-  				<ul class="dropdown-menu" id="player1weapondropdown" aria-labelledby="dropdownMenu1">
-    				<li><a href="#" class="weaponList1">Bubbles</a></li>
-    				<li><a href="#" class="weaponList1">Glitter</a></li>
-   				<li><a href="#" class="weaponList1">Fishbreath</a></li>
-   				<li><a href="#" class="weaponList1">CatSpit</a></li>
-  				</ul>
-			</div><div id="weapon1Selected"></div>`);
-			$(document).on("click", ".weaponList1", function(e){
-				let selectedWeapon = $(this).text();
-				litterBox.Player1.weaponType = new CatBotFight.Kit.WeaponType[selectedWeapon]();
+// 	CatBotFight.Player1 = new CatBotFight.Player(player1Name,selectedcatbot);
+// 	$('#weaponList1').html(`${litterBox.Player1.catbotType.name} Selected
+//     		<div class="dropdown">
+//   				<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" 	
+//   					data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+//     						Choose a Weapon
+//     				<span class="caret"></span>
+//   				</button>
+//   				<ul class="dropdown-menu" id="player1weapondropdown" aria-labelledby="dropdownMenu1">
+//     				<li><a href="#" class="weaponList1">Bubbles</a></li>
+//     				<li><a href="#" class="weaponList1">Glitter</a></li>
+//    				<li><a href="#" class="weaponList1">Fishbreath</a></li>
+//    				<li><a href="#" class="weaponList1">CatSpit</a></li>
+//   				</ul>
+// 			</div><div id="weapon1Selected"></div>`);
+// 			$(document).on("click", ".weaponList1", function(e){
+// 				let selectedWeapon = $(this).text();
+// 				litterBox.Player1.weaponType = new CatBotFight.Kit.WeaponType[selectedWeapon]();
 
-				$('#weapon1Selected').html(`${litterBox.Player1.weaponType.weapon}`);				
-			});
-		});
+// 				$('#weapon1Selected').html(`${litterBox.Player1.weaponType.weapon}`);				
+// 			});
+// 		});
 
-			$(document).on("click", ".robotList2", function(e) {
-    	let selectedCatbot = $(this).text();
-    	let player2Name = $("#player2Name").val();
+// 			$(document).on("click", ".robotList2", function(e) {
+//     	let selectedCatbot = $(this).text();
+//     	let player2Name = $("#player2Name").val();
     
-    	var selectedcatbot2 = new CatBotFight.CatHouse[selectedcatbot2]();
-    	CatBotFight.Player2 = new CatBotFight.Player(player2Name,selectedcatbot2);
+//     	var selectedcatbot2 = new CatBotFight.CatHouse[selectedcatbot2]();
+//     	CatBotFight.Player2 = new CatBotFight.Player(player2Name,selectedcatbot2);
 
 
-    	$('#weaponList2').html(`${litterBox.Player2.catBotType.name} Selected 
-    		<div class="dropdown">
-  				<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" 	
-  					data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-    						Choose a Weapon
-    				<span class="caret"></span>
-  				</button>
-  				<ul class="dropdown-menu" id="player2weapondropdown" aria-labelledby="dropdownMenu1">
-    				<li><a href="#" class="weaponList2">Bubbles</a></li>
-    				<li><a href="#" class="weaponList2">Glitter</a></li>
-   					<li><a href="#" class="weaponList2">Fishbreath</a></li>
-   					<li><a href="#" class="weaponList2">CatSpit</a></li>
-  				</ul>
-			</div><div id="weapon2Selected"></div>`);
-			$(document).on("click", ".weaponList2", function(e){
-				let selectedWeapon = $(this).text();
-				litterBox.Player2.weaponType = new CatBotFight.Kit.WeaponType[selectedWeapon]();
+//     	$('#weaponList2').html(`${litterBox.Player2.catBotType.name} Selected 
+//     		<div class="dropdown">
+//   				<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" 	
+//   					data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+//     						Choose a Weapon
+//     				<span class="caret"></span>
+//   				</button>
+//   				<ul class="dropdown-menu" id="player2weapondropdown" aria-labelledby="dropdownMenu1">
+//     				<li><a href="#" class="weaponList2">Bubbles</a></li>
+//     				<li><a href="#" class="weaponList2">Glitter</a></li>
+//    					<li><a href="#" class="weaponList2">Fishbreath</a></li>
+//    					<li><a href="#" class="weaponList2">CatSpit</a></li>
+//   				</ul>
+// 			</div><div id="weapon2Selected"></div>`);
+// 			$(document).on("click", ".weaponList2", function(e){
+// 				let selectedWeapon = $(this).text();
+// 				litterBox.Player2.weaponType = new CatBotFight.Kit.WeaponType[selectedWeapon]();
 
-				$('#weapon2Selected').html(`${litterBox.Player2.weaponType.weapon}`);				
-			});
+// 				$('#weapon2Selected').html(`${litterBox.Player2.weaponType.weapon}`);				
+// 			});
 
-   	});
+//    	});
 
-$('#fightButton').on('click' , function(){
+// $('#fightButton').on('click' , function(){
 
-		$('#player-setup').addClass('hide');
-		$('#litterBox').removeClass('hide');
-		setupLitterBox();
+// 		$('#player-setup').addClass('hide');
+// 		$('#litterBox').removeClass('hide');
+// 		setupLitterBox();
 
-	});
+// 	});
 
   	
-  	//reset button
-	$('#resetButton').on('click' , function(){
+//   	//reset button
+// 	$('#resetButton').on('click' , function(){
 
-		$('#litterBox').addClass('hide');
-		$('#player-setup').removeClass('hide');
+// 		$('#litterBox').addClass('hide');
+// 		$('#player-setup').removeClass('hide');
 
-	});
+// 	});
 
 
 
-});
+// });
 
-return LitterBox;
-})(CatBotFight || {});
+// return LitterBox;
+// })(CatBotFight || {});
 
 
 // let $Player1 = new DroneCat();
